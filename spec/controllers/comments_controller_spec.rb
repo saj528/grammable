b7941c36b6f4fe32109a2d5f1f4b1522a76bcc2b
saj528 <ajmarksboy@gmail.com>
2018-06-08 21:50:44 +0000

    pre comments

---
+++ spec/controllers/comments_controller_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe CommentsController, type: :controller do
+    describe "comments#create action" do
+    it "should allow users to create comments on grams" do
+      gram = Factorybot.create(:gram)
+      user =  Factorybot.create(:user)
+      sign_in user
+      post :create, params: {gram_id: gram.id, comment: {message: 'good gram'}}
+      expect(response).to redirect_to root_path
+      expect(gram.comments.length).to eq 1
+      expect(gram.comments.first.message).to eq 'good gram'
+    end
+
+    it "should require a user to be logged in to comment on a gram" do
+      gram = Factorybot.create(:gram)
+      user =  Factorybot.create(:user)
+      sign_in user
+      post :create, params: {gram_id: gram.id, comment:{message: 'good gram'}}
+      expect(response).to redirect_to new_user_session_path
+    end
+
+    it "should return http status code of not found if the gram isn't found" do
+      user = FactoryBot.create(:user)
+      sign_in user
+      post :create, params: { gram_id: 'YOLOSWAG', comment: { message: 'good gram' } }
+      expect(response).to have_http_status :not_found
+    end
+  end
+end
